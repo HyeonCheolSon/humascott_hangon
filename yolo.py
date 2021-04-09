c2b85efdc09e5bea36924d6f156c94b2082893af
Hyeoncheol Son <hyun630@gmail.com>
2021-04-10 02:24:08 +0900

    COCO yolo using opencv

--- yolo.py
+++ yolo.py
@@ -1,1 +1,53 @@
 import cv2
+import numpy as np
+
+net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+img = cv2.imread("sample1.jpg")
+img = cv2.resize(img, dsize=(416, 416), interpolation=cv2.INTER_AREA)
+height, width, channels = img.shape
+
+blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), False, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if class_id == 0:
+            print("person", confidence)
+        if confidence > 0.1:
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.45, 0.4)
+font = cv2.FONT_HERSHEY_PLAIN
+
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        color = colors[i]
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
